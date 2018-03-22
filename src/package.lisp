86b90ba77dacb71b1c9bd28bfa6af6893a0da7a8
Char <signed-char@protonmail.com>
2018-03-21 22:46:36 -0400

    Agregando archivo para reconocimiento de paquete o entre comillas modulo.

---
+++ src/package.lisp
@@ -0,0 +1,10 @@
+;;;; Copyright (c) 2018 Char
+;;;; All right reserved.
+;;;; See the LICENSE file for terms of use and distribution.
+
+(in-package :cl-user)
+(defpackage :csv-a-kml
+  (:nickname :cl-csv-to-kml)
+  (:use :cl)
+  (:export :csv-a-kml))
+(in-package :csv-a-kml)
